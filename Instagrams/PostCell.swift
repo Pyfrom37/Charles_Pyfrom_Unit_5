28e9a02850de6dca3896294c20d1743f118010c3
Pyfrom37 <89821499+Pyfrom37@users.noreply.github.com>
2022-10-11 21:49:27 -0400

    Add Unit 5
    added unit 5

---
+++ Instagrams/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Instagrams
+//
+//  Created by cpyfrom2014 on 10/11/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Instagrams' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks 
+  # Pods for Instagrams
+  use_frameworks!
+  pod 'AFNetworking'
+  pod 'BDBOAuth1Manager'
+  pod 'AlamofireImage'
+  pod 'Parse'
+end
